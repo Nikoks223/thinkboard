34d790f20e66f3adbeb6086da732b8a8d3928803
Nikola <nikolaefremovski8@gmail.com>
2025-06-15 23:55:28 +0200

    initial commit

---
+++ frotnend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+
+const App = () => {
+  return (
+    <div
+      className="min-h-screen w-full"
+      style={{
+        background: "radial-gradient(125% 125% at 50% 10%, #000 60%, #00FF9D40 100%)",
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
